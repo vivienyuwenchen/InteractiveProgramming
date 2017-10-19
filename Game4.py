7881c0995373ad763ec8b8a815bbeb40b9ab0d88
Vivien Chen <vivienyuwenchen@gmail.com>
2017-10-19 17:12:51 -0400

    Add player and keyboard control

--- Game4.py
+++ Game4.py
@@ -1,62 +1,108 @@
 import os, sys
 import pygame
 from random import randint
-from misc import Obstacle, colors
+from misc import Obstacle, Player, colors, jump
 
 
 class Game4Main:
     """The Main Game4 Class - This class handles the main
     initialization and creating of the Game."""
 
-    def __init__(self, width=360,height=240):
-        """Initialize"""
-        """Initialize PyGame"""
+    def __init__(self, width=600,height=480):
+        # initialize pygame
         pygame.init()
-        """Set the window Size"""
+        # set window size
         self.width = width
         self.height = height
-        """Create the Screen"""
+        # create screen
         self.screen = pygame.display.set_mode((self.width
                                                , self.height))
-
+        # initialize clock
         self.clock = pygame.time.Clock()
 
 
     def mainLoop(self):
         done = False
 
-        rect_side = 10
-        rect_x = self.width
-        rect_y = self.height - rect_side
+        # initialize player
+        jumping = False
+        frames = 0
+        startjump = 0
+        x_min = 0
+        x_max = 200
+        play_len = 25
+        play_x = 100
+        play_y = self.height - play_len
 
+        # initialize obstacle length, x and y coordinates
+        obs_len = 25
+        obs_x = self.width
+        obs_y = self.height - obs_len
+        # create list of obstacles with first obstacle
+        obstacles = [Obstacle(obs_x, obs_y, obs_len, self.screen)]
+
+        # initialize time variables
         prev_time = 0
-        obstacles = [Obstacle(rect_x, rect_y, rect_side, self.screen)]
-        delta = 500
+        obs_dt = 500
 
+        # main event loop
         while not done:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     done = True
 
-            self.screen.fill(colors['BLACK'])
+            # keyboard stuff
+            pressed = pygame.key.get_pressed()
+            if pressed[pygame.K_UP]:
+                if frames - startjump > 20:
+                    jumping = True
+                    startjump = frames
+            if pressed[pygame.K_LEFT]:
+                if play_x > x_min:
+                    play_x -= 5
+            if pressed[pygame.K_RIGHT]:
+                if play_x < x_max:
+                    play_x += 5
 
+            # refresh screen
+            self.screen.fill(colors['BLACK'])
+            #pygame.draw.rect(self.screen, colors['RED'], [0,self.height-(self.height/5),self.width,self.height/5])
+
+            jumpclock = frames%21
+            jumpover = False
+
+            if jumping == True:
+                play_y = jump(frames-startjump+1,self.height-125, self.height)[0]
+                jumpover = jump(frames-startjump+1,self.height-125, self.height)[1]
+            if jumpover == True:
+                jumping = False
+
+            pygame.draw.rect(self.screen, colors['WHITE'], [play_x,play_y,play_len,play_len])
+
+            # update current time
             current_time = pygame.time.get_ticks()
 
-            if current_time - prev_time > delta:
-                obstacles.append(Obstacle(rect_x, rect_y, rect_side, self.screen))
+            # generate obstacle at random time
+            if current_time - prev_time > obs_dt:
+                obstacles.append(Obstacle(obs_x, obs_y, obs_len, self.screen))
                 prev_time = current_time
-                delta = randint(500, 3000)
+                obs_dt = randint(500, 2000)
 
+            # move each obstacle forward
             for obstacle in obstacles:
                 obstacle.moveForward()
 
+            # remove obstacle from list if off screen
             obstacles = [obstacle for obstacle in obstacles if not obstacle.isGone()]
-            #print(obstacles)
 
+#            if player.isDead():
+#                pygame.image.load("game_over.png")
+
+            # update screen
             pygame.display.flip()
+            frames+=1
             self.clock.tick(60)
 
 
 if __name__ == "__main__":
-    MainWindow = Game4Main()
-    MainWindow.mainLoop()
+    Game4Main().mainLoop()
--- misc.py
+++ misc.py
@@ -1,6 +1,7 @@
 import os, sys
 import pygame
 
+
 colors = {'BLACK': (0, 0, 0),
             'WHITE' : (255, 255, 255),
             'GREEN' : (0, 255, 0),
@@ -8,17 +9,54 @@ colors = {'BLACK': (0, 0, 0),
             'BLUE' : (0, 0, 255),
             }
 
+
 class Obstacle:
 
-    def __init__(self, rect_x, rect_y, rect_side, screen):
-        self.rect_x = rect_x
-        self.rect_y = rect_y
-        self.rect_side = rect_side
+    def __init__(self, obs_x, obs_y, obs_len, screen):
+        self.obs_x = obs_x
+        self.obs_y = obs_y
+        self.obs_len = obs_len
         self.screen = screen
 
+    def __repr__(self):
+        return 'Obstacle({}, {}, {}, {})'.format(self.obs_x, self.obs_y, self.obs_len, self.screen)
+
     def moveForward(self):
-        self.rect_x -= 2
-        pygame.draw.rect(self.screen, colors['BLUE'], [self.rect_x, self.rect_y, self.rect_side, self.rect_side])
+        self.obs_x -= 2
+        pygame.draw.rect(self.screen, colors['BLUE'], [self.obs_x, self.obs_y, self.obs_len, self.obs_len])
 
     def isGone(self):
-        return self.rect_x < 0
+        return self.obs_x < -self.obs_len
+
+
+class Player:
+
+    def __init__(self, play_x, play_y, play_len, screen):
+        self.play_x = play_x
+        self.play_y = play_y
+        self.play_len = play_len
+        self.screen = screen
+
+#    def __repr__(self):
+#        return 'Player({}, {}, {}, {})'.format(self.rect_x, self.rect_y, self.rect_side, self.screen)
+
+def jump(ctime, startloc, height):
+    """
+    Changes the y position up one frame
+    #for i in range(21):
+        #print(jump(i,0)[1])
+    """
+    over = False
+    h = 100
+    t = 20
+    b = startloc
+    c = t/2
+    a =  h/((t/2)**2)
+    x = (ctime%20)
+    play_y = ((a*(x - c)**2)+b)
+
+
+    if (x == 0):
+        over = True
+    return [play_y, over]
+        #pygame.draw.rect(self.screen, colors['BLUE'], [self.obs_x, self.obs_y, self.obs_len, self.obs_len])
