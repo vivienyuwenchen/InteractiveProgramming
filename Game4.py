ae5ded336279a66279897405fa96f8a257315f72
Vivien Chen <vivienyuwenchen@gmail.com>
2017-10-21 12:05:43 -0400

    Add player class, collision detection, and game over

--- Game4.py
+++ Game4.py
@@ -8,31 +8,24 @@ class Game4Main:
     """The Main Game4 Class - This class handles the main
     initialization and creating of the Game."""
 
-    def __init__(self, width=600,height=480):
+    def __init__(self, width=480,height=360):
         # initialize pygame
         pygame.init()
         # set window size
         self.width = width
         self.height = height
         # create screen
-        self.screen = pygame.display.set_mode((self.width
-                                               , self.height))
-        # initialize clock
+        self.screen = pygame.display.set_mode((self.width, self.height))
+        # create clock
         self.clock = pygame.time.Clock()
 
 
     def mainLoop(self):
-        done = False
-
         # initialize player
-        jumping = False
-        frames = 0
-        startjump = 0
-        x_min = 0
-        x_max = 200
         play_len = 25
         play_x = 100
         play_y = self.height - play_len
+        player = Player(play_x, play_y, play_len, self.screen)
 
         # initialize obstacle length, x and y coordinates
         obs_len = 25
@@ -46,38 +39,26 @@ class Game4Main:
         obs_dt = 500
 
         # main event loop
-        while not done:
+        while 1:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    done = True
+                    sys.exit()
 
-            # keyboard stuff
+            # check keyboard
             pressed = pygame.key.get_pressed()
             if pressed[pygame.K_UP]:
-                if frames - startjump > 20:
-                    jumping = True
-                    startjump = frames
+                player.jump()
             if pressed[pygame.K_LEFT]:
-                if play_x > x_min:
-                    play_x -= 5
+                player.moveLeft()
             if pressed[pygame.K_RIGHT]:
-                if play_x < x_max:
-                    play_x += 5
+                player.moveRight()
 
             # refresh screen
             self.screen.fill(colors['BLACK'])
             #pygame.draw.rect(self.screen, colors['RED'], [0,self.height-(self.height/5),self.width,self.height/5])
 
-            jumpclock = frames%21
-            jumpover = False
-
-            if jumping == True:
-                play_y = jump(frames-startjump+1,self.height-125, self.height)[0]
-                jumpover = jump(frames-startjump+1,self.height-125, self.height)[1]
-            if jumpover == True:
-                jumping = False
-
-            pygame.draw.rect(self.screen, colors['WHITE'], [play_x,play_y,play_len,play_len])
+            player.update()
+            player.draw()
 
             # update current time
             current_time = pygame.time.get_ticks()
@@ -86,22 +67,39 @@ class Game4Main:
             if current_time - prev_time > obs_dt:
                 obstacles.append(Obstacle(obs_x, obs_y, obs_len, self.screen))
                 prev_time = current_time
-                obs_dt = randint(500, 2000)
+                obs_dt = randint(1000, 3000)
 
             # move each obstacle forward
             for obstacle in obstacles:
                 obstacle.moveForward()
-
+                # check for collision between player and obstacles
+                if player.isCollide(obstacle.obs_x, obstacle.obs_y, obstacle.obs_len):
+                    self.gameOver()
             # remove obstacle from list if off screen
             obstacles = [obstacle for obstacle in obstacles if not obstacle.isGone()]
 
-#            if player.isDead():
-#                pygame.image.load("game_over.png")
-
             # update screen
             pygame.display.flip()
-            frames+=1
             self.clock.tick(60)
+
+
+    def gameOver(self):
+        # main event loop
+        while 1:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+            # check keyboard
+            pressed = pygame.key.get_pressed()
+            if pressed[pygame.K_UP]:
+                self.mainLoop()
+
+            gameover = pygame.image.load("gameover.png")
+            gameover = pygame.transform.scale(gameover, (int(3*self.width/4), int(self.height/2)))
+            self.screen.blit(gameover, (int(self.width/8), int(self.height/4)))
+
+            pygame.display.flip()
 
 
 if __name__ == "__main__":
--- misc.py
+++ misc.py
@@ -36,9 +36,52 @@ class Player:
         self.play_y = play_y
         self.play_len = play_len
         self.screen = screen
+        self.speed = 5
+        self.jumpInProgress = False
+        self.v = 10
+        self.m = 2.5
+        self.floor = play_y
 
-#    def __repr__(self):
-#        return 'Player({}, {}, {}, {})'.format(self.rect_x, self.rect_y, self.rect_side, self.screen)
+    def draw(self):
+        pygame.draw.rect(self.screen, colors['WHITE'], [self.play_x, self.play_y, self.play_len, self.play_len])
+
+    def moveRight(self):
+        if self.play_x < 100:
+            self.play_x += self.speed
+
+    def moveLeft(self):
+        if self.play_x > 0:
+            self.play_x -= self.speed
+
+    def jump(self):
+        self.jumpInProgress = True
+
+    def update(self):
+        if self.jumpInProgress:
+            dy = self.m * self.v
+
+            self.play_y -= dy
+            self.v -= .75
+
+            if self.play_y >= self.floor:
+                self.play_y = self.floor
+                self.jumpInProgress = False
+                self.v = 10
+
+    def isCollide(self, obs_x, obs_y, obs_len):
+        obs_x0 = obs_x
+        obs_x1 = obs_x + obs_len
+        obs_y0 = obs_y
+        obs_y1 = obs_y + obs_len
+        play_x0 = self.play_x
+        play_x1 = self.play_x + self.play_len
+        play_y0 = self.play_y
+        play_y1 = self.play_y + self.play_len
+        if play_x0 in range(obs_x0, obs_x1) or play_x1 in range(obs_x0, obs_x1):
+            if play_y0 in range(obs_y0, obs_y1) or play_y1 in range(obs_y0, obs_y1):
+                return True
+        return False
+
 
 def jump(ctime, startloc, height):
     """
@@ -59,4 +102,3 @@ def jump(ctime, startloc, height):
     if (x == 0):
         over = True
     return [play_y, over]
-        #pygame.draw.rect(self.screen, colors['BLUE'], [self.obs_x, self.obs_y, self.obs_len, self.obs_len])
